7ddd916a98c6896a2ceed2ef720ab7efa70cafe2
Shogun19 <ayoubgoussem@tuta.io>
2024-05-04 19:36:36 +0200

    update 2

--- app/src/main/java/com/example/tp2/ActivityOne.java
+++ app/src/main/java/com/example/tp2/ActivityOne.java
@@ -3,15 +3,33 @@ package com.example.tp2;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.widget.TextView;
 
 public class ActivityOne extends AppCompatActivity {
 
-    //here
+    private SensorManager sensorManager;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        textView = findViewById(R.id.textView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Sensor gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if (gyroscopeSensor == null) {
+            textView.setText("Le gyroscope n'est pas disponible. Certaines fonctionnalités peuvent ne pas fonctionner.");
+        } else {
+            textView.setText("Le gyroscope est disponible. Toutes les fonctionnalités sont opérationnelles.");
+        }
+
         Button returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(v -> finish());
     }
+
 }
