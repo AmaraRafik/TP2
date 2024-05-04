5ae4a3dc69061e7c44146066b383e2ce46a02b3b
Shogun19 <ayoubgoussem@tuta.io>
2024-05-04 19:16:15 +0200

    new idea

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -1,53 +1,32 @@
 package com.example.tp2;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.widget.ArrayAdapter;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private SensorManager sensorManager;
-    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        SensorAdapter adapter = new SensorAdapter(sensors);
-        listView.setAdapter(adapter);
+        setupButton(R.id.button1, null);
+        setupButton(R.id.button2, null);
+        setupButton(R.id.button3, null);
+        setupButton(R.id.button4, null);
+        setupButton(R.id.button5, null);
+        setupButton(R.id.button6, null);
+        setupButton(R.id.button7, null);
     }
 
-    private class SensorAdapter extends ArrayAdapter<Sensor> {
-        SensorAdapter(List<Sensor> sensors) {
-            super(MainActivity.this, R.layout.sensor_item, sensors);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.sensor_item, parent, false);
-            }
-            TextView textViewName = convertView.findViewById(R.id.textViewSensorName);
-            TextView textViewVersion = convertView.findViewById(R.id.textViewSensorVersion);
-
-            Sensor sensor = getItem(position);
-            textViewName.setText(sensor.getName());
-            textViewVersion.setText("Version: " + sensor.getVersion());
-
-            return convertView;
-        }
+    private void setupButton(int buttonId, Class activityClass) {
+        Button button = findViewById(buttonId);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, activityClass);
+            startActivity(intent);
+        });
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "TP2"
 include(":app")
- 
