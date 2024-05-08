eaeb21bd8d0aa18720ab5baf238a7736a2e58446
Amara Rafik <rafik.amara@etu.umontpellier.fr>
2024-05-08 16:07:23 +0200

    End

--- app/src/main/java/com/example/tp2/ActivityFive.java
+++ app/src/main/java/com/example/tp2/ActivityFive.java
@@ -1,16 +1,94 @@
 package com.example.tp2;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
-public class ActivityFive extends AppCompatActivity {
+public class ActivityFive extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private boolean isFlashlightOn = false;
+    private CameraManager cameraManager;
+    private String cameraId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_one);
+        setContentView(R.layout.activity_five);
+
+        RelativeLayout mainLayout = findViewById(R.id.main_layout);
+
+        // Setup return button
         Button returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(v -> finish());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        float magnitude = (float) Math.sqrt(x * x + y * y + z * z);
+
+        //Log.d(TAG, "mag = " + magnitude);
+
+
+        if (magnitude > 13) {
+            toggleFlashlight();
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // No action needed
+    }
+
+    private void toggleFlashlight() {
+        Log.d(TAG, "Flash On");
+
+        try {
+            isFlashlightOn = !isFlashlightOn;
+            cameraManager.setTorchMode(cameraId, isFlashlightOn);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/tp2/ActivityThree.java
+++ app/src/main/java/com/example/tp2/ActivityThree.java
@@ -1,16 +1,92 @@
 package com.example.tp2;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
-public class ActivityThree extends AppCompatActivity {
+public class ActivityThree extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = ActivityThree.class.getSimpleName();
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private RelativeLayout mainLayout;
+    private float previousX, previousY, previousZ;
+    private boolean isFirstReading = true;
+
+    private static final float THRESHOLD_LOW = 2.0f;
+    private static final float THRESHOLD_MEDIUM = 7.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_three);
+
+        mainLayout = findViewById(R.id.main_layout);
+
         Button returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(v -> finish());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (accelerometer != null) {
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+
+
+        float distance = 0;
+        if (!isFirstReading) {
+            distance = (float) Math.sqrt(Math.pow(x - previousX, 2) + Math.pow(y - previousY, 2) + Math.pow(z - previousZ, 2));
+        } else {
+            isFirstReading = false;
+        }
+
+        previousX = x;
+        previousY = y;
+        previousZ = z;
+
+
+        if (distance < THRESHOLD_LOW) {
+            mainLayout.setBackgroundColor(Color.GREEN);
+        } else if ((distance < THRESHOLD_MEDIUM) && (distance > THRESHOLD_LOW)) {
+            mainLayout.setBackgroundColor(Color.BLACK);
+        } else {
+            mainLayout.setBackgroundColor(Color.RED);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
